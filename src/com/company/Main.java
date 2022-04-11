8b5b291dff6c4b56404eed6d7b30cd8b969e9d2e
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-04-11 10:40:25 +0200

    no acabado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         char[] tablero;
-        char elementos = 0;
         int extensionTablero;
         Scanner entrada = new Scanner(System.in);
 
@@ -15,21 +14,43 @@ public class Main {
         extensionTablero = pedirNumeroDeExtension(entrada, 5, 15,"¿Cuánta extension quieres que tenga el tablero? [De 5 a 15]: ");
         System.out.println();
 
-        tablero = generarTablero(elementos);
+        tablero = generarTablero(extensionTablero);
 
-
+        mostrarTablero(tablero);
     }
 
-    private static char[] generarTablero(int elementos){
-        char[] tablero = new char[elementos];
+    private static void mostrarTablero(char[] tablero) {
+        System.out.println(tablero);
+    }
 
-        for (int i = 0; i < tablero.length; i++){
-            elementos = (int)(Math.random() * 1 + 1);
+    private static char[] generarTablero(int tam){
+        char[] tablero = new char[tam];
+        int n, bombas;
+
+        for (int i = 0; i < tablero.length; i++) {
+            n = (int) (Math.random() * 2);
+            if (n == 0) {
+                tablero[i] = '0';
+            } else {
+                tablero[i] = '*';
+            }
         }
-        if (elementos == 0) {
-            elementos = '*';
-        } else if (elementos == 1) {
-            elementos = ' ';
+
+
+        for (int i = 0; i < tablero.length; i++) {
+            if (tablero[0] == '0') {
+                bombas = 0;
+            }
+            if (tablero[i] == '0') {
+                bombas = 0;
+                if (tablero[i - 1] == '*') {
+                    bombas++;
+                }
+                if (tablero[i + 1] == '*'){
+                    bombas++;
+                }
+                tablero[i] = String.valueOf(bombas).charAt(0);
+            }
         }
 
         return tablero;
